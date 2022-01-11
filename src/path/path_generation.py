4a566989026d47e77acf243b2088b34a5fad5034
Frederic Dlugi <frederic.dlugi@student.uni-luebeck.de>
2022-01-11 10:28:09 +0100

    update path generation to be actual movement of the bike model

--- src/path/path_generation.py
+++ src/path/path_generation.py
@@ -19,3 +19,12 @@ def generate_path(waypoints: np.ndarray, num_steps: int, s=0.):
     out = interpolate.splev(x, tck)
 
     return np.array(out).T
+
+def generate_path(bike, num_steps: int):
+    inputs = random.normal(size=(num_steps - 1, 2))
+    path = np.zeros((num_steps, 2))
+    bike.reset()
+    for i in range(1, num_steps):
+        bike.step(* inputs[i - 1, :])
+        path[i, :] = [bike.xc, bike.yc]
+    return path

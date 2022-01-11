09172c17f69d210f4b4241cb299c151feec05fe7
Frederic Dlugi <frederic.dlugi@student.uni-luebeck.de>
2022-01-11 10:26:12 +0100

    update bicycle model to not set speed but set accelaration

--- src/models/kinematric_bicycle_model.py
+++ src/models/kinematric_bicycle_model.py
@@ -6,30 +6,36 @@ import numpy as np
 
 class Bicycle():
     def __init__(self, lr=1.2, lf=0.8, dt=0.01):
-        self.xc = 0
-        self.yc = 0
-        self.theta = 0
-        self.delta = 0
-        self.beta = 0
+        self.reset()
 
         self.L = lf + lr
         self.lr = lr
+        self.a_max = 5
         self.w_max = 1.22
 
         self.dt = dt
 
     def reset(self):
+        self.v = 0
         self.xc = 0
         self.yc = 0
         self.theta = 0
         self.delta = 0
         self.beta = 0
 
-    def step(self, v, w):
-        xc_dot = v * cos(self.theta + self.beta)
-        yc_dot = v * sin(self.theta + self.beta)
-        theta_dot = v * cos(self.beta) * tan(self.delta) / self.L
+    def step(self, a, w):
+        """
+        param a:
+        accelaration
+        param w:
+        steering change
+        """
+        v_dot = max(-self.a_max, min(a, self.a_max))
+        xc_dot = self.v * cos(self.theta + self.beta)
+        yc_dot = self.v * sin(self.theta + self.beta)
+        theta_dot = self.v * cos(self.beta) * tan(self.delta) / self.L
         delta_dot = max(-self.w_max, min(self.w_max, w))
+        self.v += v_dot * self.dt
         self.xc += xc_dot * self.dt
         self.yc += yc_dot * self.dt
         self.theta += theta_dot * self.dt
@@ -38,8 +44,8 @@ class Bicycle():
 
     @property
     def state(self):
-        return np.array([self.xc, self.yc, self.theta, self.delta, self.beta]).astype(float)
+        return np.array([self.v, self.xc, self.yc, self.theta, self.delta, self.beta]).astype(float)
 
     @state.setter
     def state(self, value):
-        self.xc, self.yc, self.theta, self.delta, self.beta = value
+        self.v, self.xc, self.yc, self.theta, self.delta, self.beta = value
